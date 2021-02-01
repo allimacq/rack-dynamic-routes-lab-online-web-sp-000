916b8bd67a5246e8b90dd7cb47384a2111505b86
Allison _ <allimacq@gmail.com>
2021-02-01 22:48:07 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,11 +1,11 @@
 class Item
   attr_accessor :name, :price
 
-  @@items = [ ]
+  @@item = [ ]
 
   def initialize(name,price)
     @name = name
     @price = price
-    @@items << self
+    @@item << self
   end
 end
