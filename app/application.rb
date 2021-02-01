6703de5b26348cb3b9ed4c9e3a672c4645d2591d
Allison _ <allimacq@gmail.com>
2021-02-01 23:00:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,17 +5,17 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.params["q"]
-      p item_name
-      #item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
       
-      if item == nil
-        resp.write "Item not found"
-        resp.status = 400
-      else
-        resp.write item.price
-      end
+    
+      #item_name = req.path.split("/items/").last
+      #item = @@items.find{|i| i.name == item_name}
+      
+      #if item == nil
+        #resp.write "Item not found"
+        #resp.status = 400
+      #else
+        #resp.write item.price
+      #end
       
     else
       resp.write "Route not found"
