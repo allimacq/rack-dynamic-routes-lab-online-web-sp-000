236b449897e534cd8fcb457b5c637e40ba9730be
Allison _ <allimacq@gmail.com>
2021-02-01 22:29:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
   def call(env)
     resp - Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path !="/items/<ITEM NAME>"
+      resp.write "Route not found"
+      resp.status = 404
   end
   
 end
