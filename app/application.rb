60fdcbd2cda0d4894fae87370230cd1e2f1dc79d
Allison _ <allimacq@gmail.com>
2021-02-01 22:39:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       p item_name
+      p @@items
       p @@items.include?(item_name)
       
       #if @@items.include?(item_name)
