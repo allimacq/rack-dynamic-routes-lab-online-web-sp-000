6ca84c5ca738fbfa97318910949de36cb9aaa866
Allison _ <allimacq@gmail.com>
2021-02-01 22:35:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,19 @@
 class Application
   
   def call(env)
-    resp - Rack::Response.new
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      
+      if @@items.include?(item_name)
+        p item_name
+      else
+        resp.write "Item is not there"
+        resp.status = 400
+      end
+      
     else
       resp.write "Route not found"
       resp.status = 404
