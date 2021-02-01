4f149157b255951baa90986eee9315def3e0f179
Allison _ <allimacq@gmail.com>
2021-02-01 22:58:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.params["q"]
+      p item_name
       #item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       
